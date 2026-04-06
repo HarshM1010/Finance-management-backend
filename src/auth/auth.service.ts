fab6b772ca99f5512411132a17881baed184a2c9
HarshM1010 <mane36429@gmail.com>
2026-04-06 10:40:34 +0530

    added dto in auth and modified .gitignore file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -10,9 +11,9 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async login(email: string, password: string) {
+  async login(loginDto: LoginDto) {
     const user = await this.prisma.user.findUnique({
-      where: { email },
+      where: { email: loginDto.email },
     });
 
     if (!user) throw new UnauthorizedException('Invalid credentials');
@@ -25,7 +26,7 @@ export class AuthService {
       throw new UnauthorizedException('Account inactive');
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(loginDto.password, user.password);
     if (!isMatch) throw new UnauthorizedException('Invalid credentials');
 
     const payload = {
